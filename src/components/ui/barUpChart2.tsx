a87f0861937dea8c6649a0ba3d9d8ce6c4a3aa78
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-08-26 13:46:23 -0300

    updated

--- src/components/ui/barUpChart2.tsx
+++ src/components/ui/barUpChart2.tsx
@@ -208,7 +208,7 @@ export function DailyEntriesBarChart() {
                                     cursor={false}
                                     content={
                                         <ChartTooltipContent
-                                        className="flex flex-col bg-blue-600 h-52"
+                                        className="flex flex-col bg-blue-600 h-10"
                                             labelFormatter={(value) => {
                                                 const dateValue = typeof value === 'string' ? parseISO(value) : new Date(value);
                                                 return format(dateValue, 'dd/MM/yyyy', { locale: ptBR });
